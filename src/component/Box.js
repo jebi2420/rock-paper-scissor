7e395788d42948bac64fd497cf5295943be1e5a6
jebi2420 <jebi2420@gmail.com>
2024-03-19 23:24:07 +0900

    [add] image에 alt 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -8,7 +8,7 @@ const Box = (props) => {
     <div className="box">
       <h1>{props.title}</h1>
       {/* 상황에 맞춰서 에러를 방지하기 위해 조건을 써줘야함 */}
-      <img className="item-img" alt="item image" src={props.item && props.item.img}/>
+      <img className="item-img" alt="item-image" src={props.item && props.item.img}/>
       <h2>WIN</h2>
     </div>
   )
